37af52bf7e66a86de4c8a95c7c5e696492555c1a
sountastic <paulina.gr@o2.pl>
2020-07-24 14:15:31 +0200

    JDP200703-3 updating CartController

--- src/main/java/com/kodilla/ecommercee/controller/CartController.java
+++ src/main/java/com/kodilla/ecommercee/controller/CartController.java
@@ -5,42 +5,58 @@ import com.kodilla.ecommercee.domain.OrderDto;
 import com.kodilla.ecommercee.domain.ProductDto;
 import java.util.ArrayList;
 import java.util.List;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON_VALUE;
 
 @RestController
-@RequestMapping("/v1/cart/")
+@RequestMapping("/v1/cart")
 public class CartController {
 
-  @RequestMapping(method = RequestMethod.POST, value = "createCart", consumes = APPLICATION_JSON_VALUE)
+  @PostMapping(
+      path = "/createCart",
+      consumes = APPLICATION_JSON_VALUE
+  )
   public void createCart(@RequestBody CartDto cartDto) {
-    //service.saveCart(cartMapper.mapToCart(cartDto));
   }
 
-  @RequestMapping(method = RequestMethod.GET, value = "getProducts")
-  public List<ProductDto> getProducts() {
-    //return productMapper.mapToProductDtoList(service.getAllProducts());
+  @GetMapping(
+      path = "/{cartId}/products}"
+  )
+  public List<ProductDto> getProductsByCartId(@RequestParam("cartId") Long cartId) {
     return new ArrayList<>();
   }
 
-  @RequestMapping(method = RequestMethod.PUT, value = "updateCart")
+  @PutMapping(
+      path = "/{cartId}",
+      consumes = APPLICATION_JSON_VALUE,
+      produces = APPLICATION_JSON_VALUE
+  )
   public CartDto updateCart(@RequestBody CartDto cartDto) {
-    //return cartMapper.mapToCartDto(service.saveCart(cartMapper.mapToCart(cartDto)));
     return new CartDto();
   }
 
-  @RequestMapping(method = RequestMethod.DELETE, value = "deleteProduct")
-  public void deleteProduct(@RequestParam Long productId) {
-    //service.deleteProductById(productId);
+  @DeleteMapping(
+      path = "/{cartId}/product/{productId}"
+  )
+  public void deleteProductFromCart(
+      @RequestParam("cartId") Long cartId,
+      @RequestParam("productId") Long productId) {
   }
 
-  @RequestMapping(method = RequestMethod.POST, value = "createOrder", consumes = APPLICATION_JSON_VALUE)
-  public void createOrder(@RequestBody OrderDto orderDto) {
-    //service.saveTask(orderMapper.mapToTask(orderDto));
+  @PostMapping(
+      path = "/{cartId}/order",
+      consumes = APPLICATION_JSON_VALUE
+  )
+  public void createOrderFromCart(
+      @RequestParam("cartId") Long cartId,
+      @RequestBody OrderDto orderDto) {
   }
 }
