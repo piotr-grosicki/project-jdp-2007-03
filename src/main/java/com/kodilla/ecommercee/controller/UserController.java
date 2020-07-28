7aba1c19896fc763dbdd0208c7350a4250f18ca0
Maria-Zurakowska <maria.zurakowska@gmail.com>
2020-07-28 19:24:35 +0200

    task-JDP200703-5

--- src/main/java/com/kodilla/ecommercee/controller/UserController.java
+++ src/main/java/com/kodilla/ecommercee/controller/UserController.java
@@ -2,6 +2,7 @@ package com.kodilla.ecommercee.controller;
 
 import org.springframework.web.bind.annotation.*;
 
+import javax.websocket.server.PathParam;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,30 +12,49 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 @RequestMapping("/v1/user")
 public class UserController {
 
-    @RequestMapping(method = RequestMethod.GET, value = "getUsers")
+    @RequestMapping(
+            method = RequestMethod.POST,
+            value = "",
+            consumes = APPLICATION_JSON_VALUE)
+
+    public void createUser(@RequestBody UserDto userDto) {
+
+    }
+
+    @RequestMapping(
+            method = RequestMethod.GET,
+            value = "users")
+
     public List<UserDto> getUsers(){
 
         return new ArrayList<>();
     }
 
-    @RequestMapping(method = RequestMethod.GET, value = "getUser")
-    public UserDto getUser(@RequestParam Long userId) {
+    @RequestMapping(
+            method = RequestMethod.PUT,
+            value = "",
+            consumes = APPLICATION_JSON_VALUE,
+            produces = APPLICATION_JSON_VALUE)
 
-        return new UserDto ();
-    }
-
-    @RequestMapping(method = RequestMethod.DELETE, value = "deleteUser")
-    public void deleteUser (@RequestParam Long productId){
-
-    }
-
-    @RequestMapping(method = RequestMethod.PUT,value = "updateUser")
     public UserDto updateUser(@RequestBody UserDto userDto){
         return new UserDto ();
     }
 
-    @RequestMapping(method = RequestMethod.POST, value = "createUser", consumes = APPLICATION_JSON_VALUE)
-    public void createUser(@RequestBody UserDto userDto) {
+    @RequestMapping(
+            method = RequestMethod.DELETE,
+            value = "{userId}")
 
+    public void deleteUser (
+            @PathParam("userId") Long productId){
+
+    }
+
+    @RequestMapping(
+            method = RequestMethod.GET,
+            value = "{userId}")
+    public UserDto getUser(
+            @PathParam("userId") Long userId) {
+
+        return new UserDto ();
     }
 }
