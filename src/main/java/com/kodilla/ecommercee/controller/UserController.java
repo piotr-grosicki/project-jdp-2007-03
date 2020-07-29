b45a96fb086341485c4a8fae83cfcdc8c07d26ef
Maria-Zurakowska <maria.zurakowska@gmail.com>
2020-07-29 11:59:35 +0200

    task-JDP200703-5 change of @ReqestMapping

--- src/main/java/com/kodilla/ecommercee/controller/UserController.java
+++ src/main/java/com/kodilla/ecommercee/controller/UserController.java
@@ -12,27 +12,21 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 @RequestMapping("/v1/user")
 public class UserController {
 
-    @RequestMapping(
-            method = RequestMethod.POST,
-            value = "",
-            consumes = APPLICATION_JSON_VALUE)
+    @PostMapping()
 
     public void createUser(@RequestBody UserDto userDto) {
 
     }
 
-    @RequestMapping(
-            method = RequestMethod.GET,
-            value = "users")
+    @GetMapping()
 
     public List<UserDto> getUsers(){
 
         return new ArrayList<>();
     }
 
-    @RequestMapping(
-            method = RequestMethod.PUT,
-            value = "",
+    @PutMapping(
+
             consumes = APPLICATION_JSON_VALUE,
             produces = APPLICATION_JSON_VALUE)
 
@@ -40,18 +34,16 @@ public class UserController {
         return new UserDto ();
     }
 
-    @RequestMapping(
-            method = RequestMethod.DELETE,
-            value = "{userId}")
+    @DeleteMapping("/{userId}")
 
     public void deleteUser (
             @PathParam("userId") Long productId){
 
     }
 
-    @RequestMapping(
-            method = RequestMethod.GET,
-            value = "{userId}")
+    @GetMapping(
+            value = "/{userId}")
+
     public UserDto getUser(
             @PathParam("userId") Long userId) {
 
