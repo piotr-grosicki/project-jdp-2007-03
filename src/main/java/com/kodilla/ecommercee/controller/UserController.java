15cf7179f38d3141be2173fb1794690577f8da58
Maria-Zurakowska <maria.zurakowska@gmail.com>
2020-08-04 19:24:01 +0200

    Changed to @PathVariable from @PathParam

--- src/main/java/com/kodilla/ecommercee/controller/UserController.java
+++ src/main/java/com/kodilla/ecommercee/controller/UserController.java
@@ -1,12 +1,7 @@
 package com.kodilla.ecommercee.controller;
-
 import org.springframework.web.bind.annotation.*;
-
-import javax.websocket.server.PathParam;
 import java.util.ArrayList;
 import java.util.List;
-
-import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
 @RequestMapping("/v1/user")
@@ -15,7 +10,6 @@ public class UserController {
     @PostMapping("")
 
     public void createUser(@RequestBody UserDto userDto) {
-
     }
 
     @GetMapping("/users")
@@ -33,12 +27,12 @@ public class UserController {
 
     @DeleteMapping("/{userId}")
 
-    public void deleteUserById (@PathParam("userId") Long productId){
+    public void deleteUserById (@PathVariable("userId") Long userId){
     }
 
     @GetMapping("/{userId}")
 
-    public UserDto getUser(@PathParam("userId") Long userId) {
+    public UserDto getUser(@PathVariable("userId") Long userId) {
 
         return new UserDto ();
     }
