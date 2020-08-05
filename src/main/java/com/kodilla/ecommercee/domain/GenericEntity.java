1eb7daa1b3f673fa1ed28dfad6c811cedcaf3ea6
Marinder <dkucharski1996@gmail.com>
2020-08-06 00:26:23 +0200

    JDP200703-13

--- src/main/java/com/kodilla/ecommercee/domain/GenericEntity.java
+++ src/main/java/com/kodilla/ecommercee/domain/GenericEntity.java
@@ -1,22 +1,13 @@
 package com.kodilla.ecommercee.domain;
+import javax.persistence.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
-@Entity
+@MappedSuperclass
 public class GenericEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    private String value;
 
     public GenericEntity() {
-    }
-
-    public String getValue() {
-        return value;
     }
 
     public Long getId() {
@@ -24,8 +15,4 @@ public class GenericEntity {
         return id;
     }
 
-    public GenericEntity(String value) {
-
-        this.value = value;
-    }
 }
